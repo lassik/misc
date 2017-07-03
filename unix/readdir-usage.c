e62fe2afd2dc944c076786ddbf320a27331e2530
Lassi Kortela <lassi@lassi.io>
2017-07-03 17:22:33 +0300

    Add unix/readdir-usage.c

---
+++ unix/readdir-usage.c
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void diesys(const char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+static void do_ent(const char *dirpath, const char *name)
+{
+    printf("%s/%s\n", dirpath, name);
+}
+
+extern void do_dir(const char *dirpath)
+{
+    DIR *handle;
+    struct dirent *d;
+    int firsterror;
+
+    if (!(handle = opendir(dirpath)))
+        goto done;
+    for (;;) {
+        errno = 0;
+        if (!(d = readdir(handle)))
+            break;
+        if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, ".."))
+            continue;
+        do_ent(dirpath, d->d_name);
+    }
+done:
+    firsterror = errno;
+    if (handle && (closedir(handle) == -1) && !firsterror)
+        firsterror = errno;
+    if (firsterror) {
+        errno = firsterror;
+        diesys("cannot list directory");
+    }
+}
+
+int main(void)
+{
+    do_dir(".");
+    return 0;
+}
