27c78610acbece45ef9e6668a33a0fd852a063b7
Lassi Kortela <lassi@lassi.io>
2018-04-29 14:06:38 +0300

    Fix error message

--- windows/binary-stdio/binary-stdio.c
+++ windows/binary-stdio/binary-stdio.c
@@ -18,7 +18,7 @@ static void die(const char *msg) {
 static void binary_stdout(void) {
 #ifdef _WIN32
   if (_setmode(_fileno(stdout), _O_BINARY) == -1) {
-    die("cannot set stdin to binary mode");
+    die("cannot set stdout to binary mode");
   }
 #endif
 }
