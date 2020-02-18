c5c68026e72feee9392a6d690ca40d917ffbd813
Lassi Kortela <lassi@lassi.io>
2020-02-18 16:36:25 +0200

    Remove unnecessary `if` from the start

--- lisp/length-tail.scm
+++ lisp/length-tail.scm
@@ -12,18 +12,17 @@
 ;; <https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_Tortoise_and_Hare>
 ;;
 (define (length-tail object)
-  (if (not (pair? object)) (values 0 object)
-      (let detect-cycle ((len 0) (slow object) (fast object))
-        (cond ((not (pair? fast))
-               (values (* 2 len) fast))
-              ((not (pair? (cdr fast)))
-               (values (+ 1 (* 2 len)) (cdr fast)))
-              ((not (and (eq? slow fast) (> len 0)))
-               (detect-cycle (+ 1 len) (cdr slow) (cdr (cdr fast))))
-              (else
-               (let find-cycle-start ((len 0) (slow object) (fast fast))
-                 (if (eq? slow fast) (values len slow)
-                     (find-cycle-start (+ len 1) (cdr slow) (cdr fast)))))))))
+  (let detect-cycle ((len 0) (slow object) (fast object))
+    (cond ((not (pair? fast))
+           (values (* 2 len) fast))
+          ((not (pair? (cdr fast)))
+           (values (+ 1 (* 2 len)) (cdr fast)))
+          ((not (and (eq? slow fast) (> len 0)))
+           (detect-cycle (+ 1 len) (cdr slow) (cdr (cdr fast))))
+          (else
+           (let find-cycle-start ((len 0) (slow object) (fast fast))
+             (if (eq? slow fast) (values len slow)
+                 (find-cycle-start (+ len 1) (cdr slow) (cdr fast))))))))
 
 ;;
 
