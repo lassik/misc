0de29e98f7889f42fabca4aa3f8c71932be79511
Lassi Kortela <lassi@lassi.io>
2020-07-26 22:40:14 +0300

    Add closefrom() emulation

--- unix/max-fd-number.c
+++ unix/max-fd-number.c
@@ -3,6 +3,29 @@
 #include <string.h>
 #include <unistd.h>
 
+static int
+closefrom(int n)
+{
+    int max;
+
+    max = sysconf(_SC_OPEN_MAX);
+    while ((n >= 0) && (n <= max)) {
+        for (;;) {
+            if (close(n) == 0) {
+                break;
+            }
+            if (errno == EBADF) {
+                break;
+            }
+            if (errno != EINTR) {
+                return -1;
+            }
+        }
+        n++;
+    }
+    return 0;
+}
+
 int
 main(void)
 {
@@ -12,5 +35,6 @@ main(void)
         fprintf(stderr, "%s\n", strerror(errno));
     }
     printf("%d\n", val);
+    closefrom(3);
     return 0;
 }
