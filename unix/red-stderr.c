f5c943545b598dae381177ba0b259e65ea186192
Lassi Kortela <lassi@lassi.io>
2020-10-26 10:49:19 +0200

    Reformat using clang-format

--- unix/red-stderr.c
+++ unix/red-stderr.c
@@ -16,110 +16,124 @@
 
 #define ESCAPE(s) "\x1b" s
 
-static const char color_red[]   = ESCAPE("[31m");
+static const char color_red[] = ESCAPE("[31m");
 static const char color_reset[] = ESCAPE("[0m");
 
-static void read_from_fd(int fd, const char *color)
+static void
+read_from_fd(int fd, const char *color)
 {
-  static char buf[4096 * 1];
-  ssize_t nr;
+    static char buf[4096 * 1];
+    ssize_t nr;
 
-  nr = read(fd, buf, sizeof(buf));  /* xxx: nonblock */
-  if(nr == -1) err(111, "read");
-  if(nr < 1) return;
-  write(STDOUT_FILENO, color, strlen(color));  /* xxx: block */
-  write(STDOUT_FILENO, buf, (size_t)nr);
-  write(STDOUT_FILENO, color_reset, strlen(color_reset));
+    nr = read(fd, buf, sizeof(buf)); /* xxx: nonblock */
+    if (nr == -1)
+        err(111, "read");
+    if (nr < 1)
+        return;
+    write(STDOUT_FILENO, color, strlen(color)); /* xxx: block */
+    write(STDOUT_FILENO, buf, (size_t)nr);
+    write(STDOUT_FILENO, color_reset, strlen(color_reset));
 }
 
 static volatile pid_t child = 0;
 
-static void handle_sigchld(int signo)
+static void
+handle_sigchld(int signo)
 {
-  pid_t pid;
-  int status;
+    pid_t pid;
+    int status;
 
-  (void)signo;
-  for(;;) {
-    pid = wait4(-1, &status, WNOHANG, 0);
-    if(pid == -1) {
-      if(errno == ECHILD) {
-        break;
-      } else if(errno == EINTR) {
-        /* nothing to do */
-      } else {
-        err(111, "wait4");
-      }
-    } else if(pid == 0) {
-      /* nothing to do */
-    } else {
-      /* announce reaped */
-      child = 0;
+    (void)signo;
+    for (;;) {
+        pid = wait4(-1, &status, WNOHANG, 0);
+        if (pid == -1) {
+            if (errno == ECHILD) {
+                break;
+            } else if (errno == EINTR) {
+                /* nothing to do */
+            } else {
+                err(111, "wait4");
+            }
+        } else if (pid == 0) {
+            /* nothing to do */
+        } else {
+            /* announce reaped */
+            child = 0;
+        }
     }
-  }
 }
 
-static void run(char **command)
+static void
+run(char **command)
 {
-  struct sigaction sa;
-  struct pollfd fds[2];
-  int stdout_pipe[2];
-  int stderr_pipe[2];
-  int r;
+    struct sigaction sa;
+    struct pollfd fds[2];
+    int stdout_pipe[2];
+    int stderr_pipe[2];
+    int r;
 
-  memset(&sa, 0, sizeof(sa));
-  sa.sa_handler = handle_sigchld;
-  sigaction(SIGCHLD, &sa, 0);
-  r = pipe(stdout_pipe);
-  if(r == 1) err(111, "pipe");
-  r = pipe(stderr_pipe);
-  if(r == 1) err(111, "pipe");
-  if ((fcntl(stdout_pipe[0], F_SETFD, FD_CLOEXEC) == -1)||
-      (fcntl(stderr_pipe[0], F_SETFD, FD_CLOEXEC) == -1)) {
-    err(111, "cannot set close-on-exec");
-  }
-  child = fork();
-  if(child == -1) err(111, "fork");
-  if(child == 0) {
-    dup2(stdout_pipe[1], STDOUT_FILENO);
-    dup2(stderr_pipe[1], STDERR_FILENO);
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigchld;
+    sigaction(SIGCHLD, &sa, 0);
+    r = pipe(stdout_pipe);
+    if (r == 1)
+        err(111, "pipe");
+    r = pipe(stderr_pipe);
+    if (r == 1)
+        err(111, "pipe");
+    if ((fcntl(stdout_pipe[0], F_SETFD, FD_CLOEXEC) == -1) ||
+        (fcntl(stderr_pipe[0], F_SETFD, FD_CLOEXEC) == -1)) {
+        err(111, "cannot set close-on-exec");
+    }
+    child = fork();
+    if (child == -1)
+        err(111, "fork");
+    if (child == 0) {
+        dup2(stdout_pipe[1], STDOUT_FILENO);
+        dup2(stderr_pipe[1], STDERR_FILENO);
+        close(stdout_pipe[1]);
+        close(stderr_pipe[1]);
+        execvp(command[0], command);
+        _exit(126);
+    }
     close(stdout_pipe[1]);
     close(stderr_pipe[1]);
-    execvp(command[0], command);
-    _exit(126);
-  }
-  close(stdout_pipe[1]);
-  close(stderr_pipe[1]);
-  memset(fds, 0, sizeof(fds));
-  fds[0].fd = stdout_pipe[0];
-  fds[0].events = POLLIN;
-  fds[1].fd = stderr_pipe[0];
-  fds[1].events = POLLIN;
-  while(child > 0) {
-    r = poll(fds, sizeof(fds) / sizeof(fds[0]), -1);
-    if(r == -1) {
-      if(errno == EINTR) {
-        continue;
-      } else {
-        err(111, "poll");
-      }
-    } else {
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = stdout_pipe[0];
+    fds[0].events = POLLIN;
+    fds[1].fd = stderr_pipe[0];
+    fds[1].events = POLLIN;
+    while (child > 0) {
+        r = poll(fds, sizeof(fds) / sizeof(fds[0]), -1);
+        if (r == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                err(111, "poll");
+            }
+        } else {
 
-      if(r == 0) break;
+            if (r == 0)
+                break;
 
-      if(fds[0].revents & POLLIN) read_from_fd(stdout_pipe[0], color_reset);
-      if(fds[1].revents & POLLIN) read_from_fd(stderr_pipe[0], color_red);
+            if (fds[0].revents & POLLIN)
+                read_from_fd(stdout_pipe[0], color_reset);
+            if (fds[1].revents & POLLIN)
+                read_from_fd(stderr_pipe[0], color_red);
 
-      if(fds[0].revents & POLLNVAL) fds[0].events = 0;
-      if(fds[1].revents & POLLNVAL) fds[1].events = 0;
-
+            if (fds[0].revents & POLLNVAL)
+                fds[0].events = 0;
+            if (fds[1].revents & POLLNVAL)
+                fds[1].events = 0;
+        }
     }
-  }
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-  if(argc < 2) errx(100, "usage: red-stderr command");
-  run(&argv[1]);
-  return(0);
+    if (argc < 2)
+        errx(100, "usage: red-stderr command");
+    run(&argv[1]);
+    return (0);
 }
