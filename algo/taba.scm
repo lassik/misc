d7175a4662ee3ea9a29288cc21e33624c6992600
Lassi Kortela <lassi@lassi.io>
2024-10-21 16:18:47 +0300

    Refactor taba
    The TABA technique can take any two lists. One is scanned forward and
    the other is scanned in reverse. Both don't have to be the same list.

--- algo/taba.scm
+++ algo/taba.scm
@@ -15,35 +15,43 @@
                       (cdr bs)))))
 
 ;; TABA (there and back again)
-(define (taba-fold init mapfun xs)
-  (letrec ((rec (lambda (tail)
-                  (if (null? tail)
-                      (values init xs)
-                      (let-values (((acc rtail)
-                                    (rec (cdr tail))))
-                        (values (mapfun acc (car tail) (car rtail))
-                                (cdr rtail)))))))
-    (let-values (((acc rtail)
-                  (rec xs)))
-      (unless (null? rtail) (error "Can't"))
-      acc)))
+(define (taba-fold combine initial-state a-list b-list)
+  (letrec ((rec (lambda (a-tail)
+                  (if (null? a-tail)
+                      (values initial-state
+                              b-list)
+                      (let-values (((state b-tail) (rec (cdr a-tail))))
+                        (values (combine state
+                                         (car a-tail)
+                                         (car b-tail))
+                                (cdr b-tail)))))))
+    (let-values (((state b-tail) (rec a-list)))
+      ;; Any leftover items in b-tail are silently ignored.
+      state)))
 
-(define (convolve-reverse xs)
-  (taba-fold '()
-             (lambda (acc a b)
+(define (convolve-reverse a-list b-list)
+  (taba-fold (lambda (result a b)
                (cons (cons a b)
-                     acc))
-             xs))
+                     result))
+             '()
+             a-list
+             b-list))
 
-(define (palindrome? xs)
-  (taba-fold #t
-             (lambda (acc a b)
-               (and acc (equal? a b)))
-             xs))
+(define (convolve-reverse-1 list)
+  (convolve-reverse list list))
+
+(define (palindrome? list)
+  (taba-fold (lambda (result a b)
+               (and result (equal? a b)))
+             #t
+             list
+             list))
 
 (for-each writeln (convolve (iota 10) (reverse (iota 10))))
 (newline)
-(for-each writeln (convolve-reverse (iota 10)))
+(for-each writeln (convolve-reverse (iota 10) (iota 10)))
+(newline)
+(for-each writeln (convolve-reverse-1 (iota 10)))
 (newline)
 (writeln (palindrome? '()))
 (writeln (palindrome? '(1)))
